5e0f7f50004299863a27fba4c4f722746d36f633
rutwikhdev <rutwikhdev@gmail.com>
2021-07-07 22:06:41 +0530

    edited starting page

--- library-client/src/App.js
+++ library-client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation';
+import { Redirect } from 'react-router-dom';
 
 import './App.css';
 
 const App = () => {
     return (
-        <Navigation/>
+        <>
+            <Navigation/>
+            <Redirect to="/rent" />
+        </>
     )
 }
 
